ee053354e6089df0fce33734827e32d2e447cb06
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-15 19:18:17 +0545

    Login Ui create

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -63,26 +63,6 @@ function Navbar() {
             Login
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 mx-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="1.5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-              />
-            </svg>
-          </Link>
-          <Link
-            href={"/signup"}
-            className="text-white mr-5 cursor-pointer hover:text-gray-200 flex items-center"
-          >
-            Signup
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
@@ -93,6 +73,26 @@ function Navbar() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+              />
+            </svg>
+          </Link>
+          <Link
+            href={"/signup"}
+            className="text-white mr-5 cursor-pointer hover:text-gray-200 flex items-center"
+          >
+            Signup
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mx-1"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
               />
             </svg>
           </Link>
